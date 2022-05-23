38b41a65ef776ed8435dbb0e173def8e8b81e1b6
unknown <emmanuelugbotor@gmail.com>
2022-05-23 05:23:34 -0700

    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import About from './pages/About';
-import Experiment from './pages/Experiment';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/comingsoon" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
-        <Route path="/experiment" element={<Experiment />} />
       </Routes>
     </React.Fragment>
   );
